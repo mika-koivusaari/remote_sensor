8c48121c43bf6fb8943674a725ed3ee3739df8f8
Mika Koivusaari <mika.koivusaari@gmail.com>
2016-10-24 13:49:42 +0300

    Added stop pin.

--- main.py
+++ main.py
@@ -6,51 +6,57 @@ import ubinascii
 from umqtt.simple import MQTTClient
 import ntptime
 
-ONEWIREPIN = 5
-
 def gettimestr():
     rtc=machine.RTC()
     curtime=rtc.datetime()
     _time="%04d" % curtime[0]+ "%02d" % curtime[1]+ "%02d" % curtime[2]+" "+ "%02d" % curtime[4]+ "%02d" % curtime[5]
     return _time
 
-f = open('config.json', 'r')
-config = ujson.loads(f.readall())
+#check if gpio4 is pulled down
+stoppin = machine.Pin(4,mode=machine.Pin.IN,pull=machine.Pin.PULL_UP)
+if stoppin.value()==0:
+    print("Pin down, stop")
+else:
+    #normal loop
+    ONEWIREPIN = 5
 
-# the device is on GPIOxx
-dat = machine.Pin(ONEWIREPIN)
+    f = open('config.json', 'r')
+    config = ujson.loads(f.readall())
 
-# create the onewire object
-ds = ds18x20.DS18X20(onewire.OneWire(dat))
+    # the device is on GPIOxx
+    dat = machine.Pin(ONEWIREPIN)
 
-# scan for devices on the bus
-roms = ds.scan()
-print('found devices:', roms)
+    # create the onewire object
+    ds = ds18x20.DS18X20(onewire.OneWire(dat))
 
-#print('temperatures:', end=' ')
-ds.convert_temp()
-time.sleep_ms(750)
-ntptime.settime()
-_time=gettimestr()
-c = MQTTClient("umqtt_client", config['MQTT_BROKER'])
-c.connect()
+    # scan for devices on the bus
+    roms = ds.scan()
+    print('found devices:', roms)
 
-#check battery voltage?
-if (config['MEASURE_VOLTAGE']=="true"):
-    adc = machine.ADC(0)
-    voltage = adc.read();
-    topic="/hardware/"+machine.unique_id().decode()+"/voltage/"
-    message=_time+" "+str(voltage)
-    c.publish(topic,message)
-    
+    #print('temperatures:', end=' ')
+    ds.convert_temp()
+    time.sleep_ms(750)
 
-#loop ds18b20 and send results to mqtt broker
-for rom in roms:
-    print("topic "+config['MQTT_TOPIC']+ubinascii.hexlify(rom).decode())
-    topic=config['MQTT_TOPIC']+ubinascii.hexlify(rom).decode()
-    print(_time)
-    print(ds.read_temp(rom))
-    message=_time+' '+str(ds.read_temp(rom))
-    c.publish(topic,message)
+    ntptime.settime()
+    _time=gettimestr()
+    c = MQTTClient("umqtt_client", config['MQTT_BROKER'])
+    c.connect()
 
-c.disconnect()
+    #check battery voltage?
+    if (config['MEASURE_VOLTAGE']=="true"):
+        adc = machine.ADC(0)
+        voltage = adc.read();
+        topic="/hardware/"+machine.unique_id().decode()+"/voltage/"
+        message=_time+" "+str(voltage)
+        c.publish(topic,message)
+
+    #loop ds18b20 and send results to mqtt broker
+    for rom in roms:
+        print("topic "+config['MQTT_TOPIC']+ubinascii.hexlify(rom).decode())
+        topic=config['MQTT_TOPIC']+ubinascii.hexlify(rom).decode()
+        print(_time)
+        print(ds.read_temp(rom))
+        message=_time+' '+str(ds.read_temp(rom))
+        c.publish(topic,message)
+
+    c.disconnect()
